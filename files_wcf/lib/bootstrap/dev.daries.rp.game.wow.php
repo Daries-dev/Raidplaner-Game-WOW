a1ae84c327df7e5f019fae278a2924bff98f47d8
xDeraiser <github@daries.dev>
2024-08-18 17:03:26 +0200

    Added attendee checking registration in event handler

--- files_wcf/lib/bootstrap/dev.daries.rp.game.wow.php
+++ files_wcf/lib/bootstrap/dev.daries.rp.game.wow.php
@@ -4,6 +4,8 @@ use rp\data\game\GameCache;
 use rp\event\character\AvailableCharactersChecking;
 use rp\event\character\CharacterAddCreateForm;
 use rp\event\event\EventCreateForm;
+use rp\event\raid\AddAttendeesChecking;
+use rp\system\event\listener\DefaultAddAttendeesChecking;
 use rp\system\event\listener\WOWAvailableCharactersChecking;
 use rp\system\event\listener\WOWCharacterAddCreateFormListener;
 use rp\system\event\listener\WOWEventCreateFormListener;
@@ -14,6 +16,7 @@ return static function (): void {
 
     $eventHandler = EventHandler::getInstance();
 
+    $eventHandler->register(AddAttendeesChecking::class, DefaultAddAttendeesChecking::class);
     $eventHandler->register(AvailableCharactersChecking::class, WOWAvailableCharactersChecking::class);
     $eventHandler->register(CharacterAddCreateForm::class, WOWCharacterAddCreateFormListener::class);
     $eventHandler->register(EventCreateForm::class, WOWEventCreateFormListener::class);
